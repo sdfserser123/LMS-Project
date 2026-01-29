2a932f9da8a29ee9bc762f8bf069beda390b8831
Bùi Minh Thái <54024053+sdfserser123@users.noreply.github.com>
2026-01-29 19:53:45 +0700

    Added: Logout button on StudentDashboard

--- prj-frontend/src/pages/student/StudentDashboard.jsx
+++ prj-frontend/src/pages/student/StudentDashboard.jsx
@@ -1,8 +1,11 @@
+import SignOut from "../../components/auth/signout"
+
 export const StudentDashboard = ({ onLogin }) => {
   return (
     <div>
       <div>
         StudentDashboard
+        <SignOut/>
       </div>
     </div>
   );
